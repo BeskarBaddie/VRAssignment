61bc746ae65422a65a35030ab92fe92aac46aa3c
Shreeyon <62268588+Shreeyon@users.noreply.github.com>
2023-10-15 17:39:59 +0200

    sound experiment
    add test cylinder

---
+++ My project/Assets/PlaySound.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{   private AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source= GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)  
+    {
+        source.Play();
+    }  
+}
